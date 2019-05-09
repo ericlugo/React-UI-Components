621b0a1ee01ab223044eed0984e8fec321acf9c4
Eric Lugo <ericlugo@users.noreply.github.com>
2019-05-08 23:17:57 -0400

    MVP COMPLETE

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
 import './Header.css';
+
+const HeaderContainer = () => {
+  return (
+    <header>
+      <ImageThumbnail />
+      <div className='text-container'>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </header>
+  );
+};
+
+export default HeaderContainer;
